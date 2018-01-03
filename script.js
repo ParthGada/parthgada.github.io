f4c3c12b877329630552a1af65f6f9503de0697e
ParthGada <parthrgada@gmail.com>
2018-01-03 14:59:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    flag = 0;
+    $("#icon").click(function(){
+        if(flag==0){
+            $("#main").css('box-shadow', 'inset 0px 0px 0px 30px black, inset 0px -70px 0px 30px black');
+            $("#main").css('background-image','img/indiaflaghomepage.jpg');
+            $("#m1").animate({top:'40%'},200);
+            $("#m2").animate({top:'40%'},200);
+            $("#m3").animate({top:'40%'},200);
+            $("#m4").animate({top:'40%'},200);
+            flag=1;
+        }  else{
+            $("#main").css('box-shadow', 'inset 0px 0px 0px 0px black, inset 0px 0px 0px 0px black');
+            $("#main").css('background-image','img/indiaflaghomepage.jpg');
+            $("#m1").animate({top:'100%'},200);
+            $("#m2").animate({top:'100100%'},200);
+            $("#m3").animate({top:'100%'},200);
+            $("#m4").animate({top:'100%'},200);
+            flag=0;
+        }
+    });
+
+});
